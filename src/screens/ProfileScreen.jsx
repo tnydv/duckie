f9b71a226509ac45b2e9abd4b2d2b9734482ef1e
tnydv <tnydvdv@gmail.com>
2024-03-14 23:12:28 -0700

    reorganized imports

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,5 +1,5 @@
-import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 import DuckImage from "../../assets/duck.png";
 

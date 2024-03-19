5b5e067ddebbd0b15d6e59cb681596978ff74ecb
tnydv <tnydvdv@gmail.com>
2024-03-18 19:21:07 -0700

    fixed like button layout, added haptics for liking/unliking a quack

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,19 +1,44 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+import * as Haptics from "expo-haptics";
+
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Item = ({ iconName, title, description, likes }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
+  const [currentLikes, setCurrentLikes] = useState(likes);
+
+  const handleVibrate = async () => {
+      try {
+        //todo: fix if statement, its backwards. but works correctly
+      if (isLiked) {
+        await Haptics.selectionAsync();
+      } else {
+        await Haptics.notificationAsync(
+          Haptics.NotificationFeedbackType.Success
+        );
+      }
+    } catch (error) {
+      console.error("Error vibrating device:", error);
+    }
+  };
 
   const handlePlayButton = () => {
     setIsPlaying(!isPlaying);
   };
 
-  const handleLikePress = () => {
+  const handleLikePress = async () => {
     setIsLiked(!isLiked);
+    setCurrentLikes((previousState) =>
+      isLiked ? previousState - 1 : previousState + 1
+    );
+
+    await handleVibrate();
   };
+
+  let iconColor = isLiked ? "#F00" : "#444";
 
   return (
     <View style={styles.container}>
@@ -31,20 +56,20 @@ const Item = ({ iconName, title, description, likes }) => {
           <Text style={styles.description}>{description}</Text>
         </View>
         <TouchableOpacity
-          style={styles.likeContainer}
+          style={[styles.likeContainer, styles.contentAndLikes]}
           onPress={handleLikePress}>
           <MaterialCommunityIcons
+            color={iconColor}
             name={isLiked ? "heart" : "heart-outline"}
             size={24}
           />
-          <Text style={styles.likes}>{likes}</Text>
+          <Text style={styles.likes}>{currentLikes}</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
 };
 
-//todo:  make heart container fixed.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -82,6 +107,10 @@ const styles = StyleSheet.create({
   likes: {
     marginLeft: 5,
     fontSize: 14,
+  },
+  contentAndLikes: {
+    flexDirection: "column",
+    flex: 0.15,
   },
 });
 

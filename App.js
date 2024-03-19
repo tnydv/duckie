5e1f999f33d4b7bedd0bc8b58ade76dde70ec185
tnydv <tnydvdv@gmail.com>
2024-03-19 00:33:02 -0700

    refactored App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -10,59 +9,55 @@ import RecordingsScreen from "./src/screens/RecordigsScreen";
 
 const Tab = createBottomTabNavigator();
 
+const screens = [
+  {
+    name: "Profile",
+    component: ProfileScreen,
+    options: {
+      tabBarIcon: ({ color, size }) => (
+        <MaterialCommunityIcons name="account" color={color} size={size} />
+      ),
+    },
+  },
+  {
+    name: "Duck",
+    component: HomeScreen,
+    options: {
+      ...{ headerShown: false }, // Hide header for HomeScreen
+      tabBarIcon: ({ color, size }) => (
+        <MaterialCommunityIcons name="duck" color={color} size={size} />
+      ),
+    },
+  },
+  {
+    name: "My Quacks",
+    component: RecordingsScreen,
+    options: {
+      tabBarIcon: ({ color, size }) => (
+        <MaterialCommunityIcons
+          name="playlist-play"
+          color={color}
+          size={size}
+        />
+      ),
+    },
+  },
+];
+
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Duck"
         screenOptions={({ route }) => ({
-          headerShown: route.name !== "Duck",
+          headerShown:
+            screens.find((screen) => screen.name === route.name)?.options?.headerShown ??
+            true,
         })}>
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="account"
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Duck"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="duck" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="My Quacks"
-          component={RecordingsScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="playlist-play"
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
+        {screens.map((screen) => (
+          <Tab.Screen key={screen.name} {...screen} />
+        ))}
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    textAlign: "center",
-  },
-});
